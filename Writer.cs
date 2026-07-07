0d34c2624687771be49bf6a08e6338df808f2468
Kitill Tilicyn <ogromniymladenec@gmail.com>
2026-07-07 22:56:12 +0300

    Создание поля и конструктора в классе Writer

--- Writer.cs
+++ Writer.cs
@@ -4,6 +4,18 @@ namespace NewsLibrary
 {
     internal class Writer
     {
+        private string _name;
 
+        public Writer(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                _name = "Неизвестный автор";
+            }
+            else
+            {
+                _name = name;
+            }
+        }
     }
 }
