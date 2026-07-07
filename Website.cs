2b0625268457eac523138bf94097b250d0da4a7a
Kitill Tilicyn <ogromniymladenec@gmail.com>
2026-07-07 22:51:32 +0300

    Создание поля и конструктора в классе Website

--- Website.cs
+++ Website.cs
@@ -1,9 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace NewsLibrary
 {
     internal class Website
     {
+        private List<string> _writers;
 
+        public Website()
+        {
+            _writers = new List<string>();
+        }
     }
 }
