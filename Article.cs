0d1d85ed34f2ec491b82011e05748d4b1fff174a
Kitill Tilicyn <ogromniymladenec@gmail.com>
2026-07-07 23:27:41 +0300

    Создание поля _title, конструктора и метода GetTitle в классе Article.

--- Article.cs
+++ Article.cs
@@ -4,5 +4,23 @@ namespace NewsLibrary
 {
     public class Article
     {
+        private string _title;
+
+        public Article(string title)
+        {
+            if (string.IsNullOrEmpty(title))
+            {
+                _title = "Без названия";
+            }
+            else
+            {
+                _title = title;
+            }
+        }
+
+        public string GetTitle()
+        {
+            return _title;
+        }
     }
 }
