a37ca79d056a52d750697f760eb90f45b6e2af65
Kitill Tilicyn <ogromniymladenec@gmail.com>
2026-07-10 13:33:49 +0300

    Класс Article добавлено поле _description и отредактирован констрктор для заполнения анного поля.

--- Article.cs
+++ Article.cs
@@ -5,17 +5,22 @@ namespace NewsLibrary
     public class Article
     {
         private string _title;
+        private string _description;
 
-        public Article(string title)
+        public Article(string title, string description)
         {
-            if (string.IsNullOrEmpty(title))
+            if (string.IsNullOrEmpty(title) && string.IsNullOrEmpty(description))
             {
                 _title = "Без названия";
+                _description = "Пустая статья";
             }
             else
             {
                 _title = title;
+                _description = description;
             }
+
+            _description = description;
         }
 
         public string GetTitle()
